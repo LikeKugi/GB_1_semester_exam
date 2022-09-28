d84c8c818fa286dc2b40d83e68033f4e2d7a45d3
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-09-28 14:30:06 +0300

    created methods to create an array with len of an element 3 or less

--- Code/Program.cs
+++ Code/Program.cs
@@ -8,9 +8,28 @@ namespace MyProgram
         {
         // Main starts here
             Console.Clear();
+            var maxLen = 3;
             var line = new ChoosingInput();
-            Console.WriteLine(line.Line);
+            var words = new ArrayOfString(line.Line);
+            Console.WriteLine("The array of strings is:");
+            PrintArray(words.Words);
+            var wordsShort = RemoveShortWordsFromArray(words.Words, maxLen);
+            Console.WriteLine("The array of words with length {0} or less charackters is: ",maxLen);
+            PrintArray(wordsShort);
         }
+        static string[] RemoveShortWordsFromArray(string[] words, int len) => words
+            .Where(n=>(n.Length<=len))
+            .ToArray();
+        static void PrintArray(string[] arr) => Console.WriteLine(string.Join(" ",arr));
     }
-    
+    class ArrayOfString
+    {
+        public string[] Words {get;init;}
+        public ArrayOfString(string line)
+        {
+            this.Words = ArrayFromString(line);
+        }
+        static string[] ArrayFromString(string line) => line
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+    }
 }
